367aafaa1101ef950a7e7600d3c75d86d2e4971a
lihang <lihang@lightgogogo.com>
2021-07-09 14:18:42 +0800

    神婆卡片简单UI

--- lib/widgets/starCard.dart
+++ lib/widgets/starCard.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:lifeaste/common/colors.dart';
 import 'package:lifeaste/common/strings.dart';
@@ -18,33 +19,62 @@ class StarCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 228,
       margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(color: GlobalColors.lightTheme, blurRadius: 18, offset: Offset(0, 8))
-        ]
+          borderRadius: BorderRadius.circular(16),
+          color: GlobalColors.darkThemeText,
+          boxShadow: [
+            BoxShadow(color: GlobalColors.shadowColor, blurRadius: 18, offset: Offset(0, 8), spreadRadius: 0)
+          ]
       ),
       child: GestureContainer(
         onTap: onTap,
         child: Column(
           children: [
+            SizedBox(height: 5,),
             Row(
               children: [
-                RoundAvatar(74, star.avatar),
-                Text(star.name, style: textStyleBold(16),),
+                SizedBox(width: 12,),
+                RoundAvatar(60, star.avatar),
+                SizedBox(width: 8,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(star.name, style: textStyleBold(16),),
+                    SizedBox(height: 3,),
+                    Container(
+                      width: 245,
+                      child: AutoSizeText(
+                        star.bio,
+                        style: textStyleNormal(12),
+                        minFontSize: 6,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                )
               ],
             ),
-            Text(star.bio, style: textStyleBold(16),),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: InnerShadowButton(
-                title: Strings.askNow,
-                height: 42,
-                onTap: onAsk,
+            SizedBox(height: 17,),
+            GestureDetector(
+              onTap: onAsk,
+              child: Container(
+                height: 41,
+                width: 311,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    color: GlobalColors.buttonBg
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  Strings.askNow,
+                  style: textStyleBold(18).copyWith(color: GlobalColors.darkThemeText),
+                ),
               ),
             ),
+            SizedBox(height: 17,),
           ],
         ),
       ),
